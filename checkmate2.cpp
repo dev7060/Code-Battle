64bfe20f7c8f12b6f76053b235a0f09fd0166336
Dev <46674147+dev7060@users.noreply.github.com>
2021-07-03 18:37:50 +0530

    Create checkmate2.cpp

---
+++ checkmate2.cpp
@@ -0,0 +1,36 @@
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+    public:
+    bool checkmate(int a, int b, int x, int y){
+        // code here
+        if((a==x) || (b==y)){
+            return 0;
+        }
+        if(abs(a-b)==abs(x-y)){
+            return 0;
+        }
+        return 1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int a, b, x, y;
+        cin >> a >> b >> x >> y;
+        Solution obj;
+        cout << obj.checkmate(a, b, x, y) << "\n";
+    }
+}
+  // } Driver Code Ends
